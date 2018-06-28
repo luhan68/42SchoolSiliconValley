0d52cd4ed740023e53d43a338c65de35f6cc5452
Lu Han <lhan@e1z1r2p3.42.us.org>
2018-06-27 22:04:56 -0700

    argc/argv

---
+++ Day06/ex02/ft_print_params.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lhan <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/06/27 15:17:44 by lhan              #+#    #+#             */
+/*   Updated: 2018/06/27 15:39:49 by lhan             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+	{
+		ft_putchar(*str);
+		str++;
+	}
+}
+
+int		main(int argc, char **argv)
+{
+	int i;
+
+	i = 1;
+	if (argc > 0)
+	{
+		while (i < argc)
+		{
+			ft_putstr(argv[i]);
+			ft_putchar('\n');
+			i++;
+		}
+	}
+	return (0);
+}
